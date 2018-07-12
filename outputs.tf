5c13839360306a8645d4438d009acf6032721215
aknysh <andriy.knysh@gmail.com>
2018-07-12 12:06:53 -0400

    Fix outputs

--- outputs.tf
+++ outputs.tf
@@ -4,21 +4,21 @@ output "registry_id" {
 }
 
 output "registry_url" {
-  value       = "${module.kops_ecr.repository_url}"
+  value       = "${module.kops_ecr.registry_url}"
   description = "Registry URL"
 }
 
 output "repository_name" {
-  value       = "${module.kops_ecr.name}"
-  description = "Registry name"
+  value       = "${module.kops_ecr.repository_name}"
+  description = "Repository name"
 }
 
 output "role_name" {
   value       = "${module.kops_ecr.role_name}"
-  description = "Assume Role name to get access registry"
+  description = "Role name to get access to the registry"
 }
 
 output "role_arn" {
   value       = "${module.kops_ecr.role_arn}"
-  description = "Assume Role ARN to get access registry"
+  description = "Role ARN to get access to the registry"
 }
